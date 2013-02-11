1e8f714710b72b5cef97548caf2a2d743ead9957
Cyril Stoller <cyril.stoller@gmail.com>
2013-02-11 22:34:04 +0100

    supports audio now, better menu, updated readme

--- AirDrum.py
+++ AirDrum.py
@@ -1,9 +1,9 @@
 '''
-AirConga
+AirDrum
 
 Copyright (C) 2013  Cyril Stoller
 
-AirConga is free software: you can redistribute it and/or modify
+AirDrum is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published
 by the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
@@ -13,46 +13,41 @@ import Leap, sys, math
 from collections import defaultdict
 import pygame.midi
 
+
 def mid_position(VectorA, VectorB):
     return VectorA + (VectorB - VectorA) / 2
 
-class CongaListener(Leap.Listener):
+class DrumListener(Leap.Listener):
     midi_out = None
+    play_midi = None
+    note_kick = 36 # sometimes: 48
+    note_snare = 40 # sometimes: 52
+    midi_channel = 0
     
-    note_snare = 52
-    note_kick = 48
-    channel = 0
-    
-    VELOCITY_THRESHOLD = -600
+    VELOCITY_THRESHOLD = -600 # m/s
     NOTE_HOLD = 40000 # microseconds
+    MULTISAMPLE_THRESHOLD = 0.5
     
     fingerdict = {}
     notes_playing = []
+    sounds = {}
     
     # DEBUG:
     file = None
 
     def on_init(self, controller):
-        print "Initialized"
-        # initialize the midi device
-        pygame.midi.init()
-        self.set_midi_device()
+        print "Initialized\n"
 
     def on_connect(self, controller):
-        print "Connected"
-        
         # DEBUG:
         self.file = open("dict_output.txt", 'w')
-
-    def on_disconnect(self, controller):
-        print "Disconnected"
 
     def on_frame(self, controller):
         # Get the most recent frame
         frame = controller.frame()
         
         # search trough all the pointables:
-        if not frame.pointables.empty:
+        if not frame.pointables.empty and self.play_midi != None:
             # DEBUG:
             self.file.write("%f\t%d" % ((frame.timestamp - controller.frame(1).timestamp)/1000000.0, frame.id))
             for pointable in frame.pointables:
@@ -73,7 +68,9 @@ class CongaListener(Leap.Listener):
                 if frame.timestamp - hit['timestamp'] > self.NOTE_HOLD:
                     # DEBUG:
                     print "\t%s off " % ("snare" if hit['note'] == self.note_snare else "kick")
-                    self.midi_out.note_off(hit['note'], 0, self.channel)
+                    
+                    if self.play_midi:
+                        self.midi_out.note_off(hit['note'], 0, self.midi_channel)
                     self.notes_playing.remove(hit['note'])
                     del_array.append(hit_id)
             # not played yet:
@@ -123,61 +120,114 @@ class CongaListener(Leap.Listener):
         self.file.write("\tKick on" if hit['note'] == self.note_kick else "\tSnare on")
         
         # calculate velocity
-        velocity = - hit['velocity'] - 500.0     # start from around zero
+        velocity = - hit['velocity'] - 500.0        # start from around zero
         if pointable.is_tool:
-            velocity = velocity / 6000.0            # if it's a tool, greater velocities can be achieved -> scale it more
+            velocity /= 6000.0                      # if it's a tool, greater velocities can be achieved -> scale it more
         else:
-            velocity = velocity / 4000.0            # if it's a finger, don't scale it as much as a tool
+            velocity /= 4000.0                      # if it's a finger, don't scale it as much as a tool
         velocity = min(velocity, 1.0)               # hard limit to 0 < velocity < 1
         velocity = max(0.0, velocity)
         velocity = math.pow(velocity, 0.4)          # it feels as if it's a bit exponential -> calculate that out
-        velocity = velocity * 126.0 + 1.0           # scale up again to 1 - 127
         
-        # play note on midi out
-        self.midi_out.note_on(hit['note'], int(velocity), self.channel)
-        # DEBUG:
-        print "\t%s on with velocity: %.2f -> %d" % (("snare" if hit['note'] == self.note_snare else "kick"), hit['velocity'], velocity)
+        # play the actual hit
+        if self.play_midi:
+            velocity = velocity * 126.0 + 1.0           # scale up again to 1 - 127
+            # play note on midi out
+            # DEBUG:
+            print "\t%s on with velocity: %.2f m/s -> Midi %d" % (("snare" if hit['note'] == self.note_snare else "kick"), hit['velocity'], velocity)
+            self.midi_out.note_on(hit['note'], int(velocity), self.midi_channel)
+        else:
+            # play audio sound
+            # determine which multisample (2 times multisamples each)
+            if velocity < self.MULTISAMPLE_THRESHOLD:
+                # soft sample
+                multisample = 0
+                #velocity /= self.MULTISAMPLE_THRESHOLD
+            else:
+                # hard sample
+                multisample = 1
+                velocity -= self.MULTISAMPLE_THRESHOLD/3
+                velocity /= (1-self.MULTISAMPLE_THRESHOLD/3)
+            # DEBUG:
+            print "\t%s (%s) on with velocity: %.2f m/s -> Volume %.2f" % (("snare" if hit['note'] == self.note_snare else "kick"), ("hard" if multisample else "soft"), hit['velocity'], velocity)
+            self.sounds[hit['note']][multisample].set_volume(velocity)
+            self.sounds[hit['note']][multisample].play()
         
         # register note in array:
         self.notes_playing.append(hit['note'])
 
-    def set_midi_device(self):
-        c = pygame.midi.get_count()
-        id_device_from_settings = -1
-        #print '%s midi devices found' % c
-        for i in range(c):
-            print '%s name: %s input: %s output: %s opened: %s' % (pygame.midi.get_device_info(i))
-            if pygame.midi.get_device_info(i)[1] == "Fast Track Pro MIDI Out":
-                # if the device exists in the computers list, take that!
-                id_device_from_settings = i
-        
-        print 'Default is %s' % pygame.midi.get_device_info(pygame.midi.get_default_output_id())[1]
-        
-        if id_device_from_settings <> -1:
-            midi_device_id = id_device_from_settings
-            print 'MIDI device "%s" found. Connecting.' % pygame.midi.get_device_info(id_device_from_settings)[1]
-        else:
-            # if it was not in the list, take the default one
-            midi_device_id = pygame.midi.get_default_output_id()
-            print 'Warning: No MIDI device named "%s" found. Choosing the system default ("%s").' % ("Fast Track Pro MIDI Out", pygame.midi.get_device_info(self.midi_device)[1])
-        
-        if pygame.midi.get_device_info(midi_device_id)[4] == 1:
-            print 'Error: Can''t open the MIDI device - It''s already opened!'
-            
-        self.midi_out = pygame.midi.Output(midi_device_id)
 
+    def init_midi(self):
+        self.play_midi = True
+        # initialize the midi device
+        pygame.midi.init()
+        
+        # list all Midi output devices
+        print ""
+        c = pygame.midi.get_count()
+        default = pygame.midi.get_default_output_id()
+        for i in range(c):
+            if pygame.midi.get_device_info(i)[3] == 1:
+                #print '%s name: %s input: %d output: %d opened: %d' % (pygame.midi.get_device_info(i))
+                print '%d: "%s"%s' % (i, pygame.midi.get_device_info(i)[1], " (default)" if i == default else "")
+        
+        # let user choose one
+        choice = raw_input("Choose a Midi output (press enter for default): ")
+        choice = int(choice) if choice.isdigit() else -1
+        if choice in range(c) and pygame.midi.get_device_info(choice)[3] == 1:
+            # valid?
+            print 'Connecting to "%s".\n' % pygame.midi.get_device_info(choice)[1]
+        else:
+            # invalid? (if it was not in the list, not an output or just [enter]) take the default one
+            choice = pygame.midi.get_default_output_id()
+            print 'Connecting to the system default ("%s").\n' % pygame.midi.get_device_info(default)[1]
+        
+        # if it's the Microsoft GS Wavetable Synth, choose Midi channel 10 to play drums
+        if pygame.midi.get_device_info(choice)[1] == "Microsoft GS Wavetable Synth":
+            print "Since the Microsoft GS Wavetable Synth was selected, Midi channel has switched to 10"
+            self.midi_channel = 9
+        
+        # try to open the device
+        if pygame.midi.get_device_info(choice)[4] == 1:
+            print "Error: Can't open the MIDI device - It's already opened!"
+        else:
+            self.midi_out = pygame.midi.Output(choice)
+
+    def init_audio(self):
+        self.play_midi = False
+        
+        print "\nSetting up audio files.\n"
+        pygame.mixer.pre_init(44100, -16, 2, 512)
+        pygame.mixer.init()
+        pygame.init()
+        self.sounds[self.note_snare] = (pygame.mixer.Sound('Snare_soft.ogg'), pygame.mixer.Sound('Snare_hard.ogg'))
+        self.sounds[self.note_kick] = (pygame.mixer.Sound('Kick_soft.ogg'), pygame.mixer.Sound('Kick_hard.ogg'))
 
 def main():
     # Create a listener and controller
-    listener = CongaListener()
+    listener = DrumListener()
     controller = Leap.Controller()
     
     # Have the sample listener receive events from the controller
     controller.add_listener(listener)
     
+    # let user choose betweed playing audio sound or Midi notes
+    print "0: Audio (default)"
+    print "1: Midi"
+    choice = raw_input("Choose between Audio or Midi output: (press enter for default): ")
+    choice = int(choice) if choice.isdigit() else -1
+    
+    choice
+    
+    if choice == 1:
+        listener.init_midi()
+    else:
+        listener.init_audio()
+    
     # Keep this process running until Enter is pressed
     print "Press Enter to quit..."
     sys.stdin.readline()
+    
     print "Exiting..."
     
     # DEBUG:
@@ -188,7 +238,7 @@ def main():
     #listener.midi_out.close()
     #pygame.midi.quit()
 
-    # app closes: Remove the listener
+    # App closes: Remove the listener
     controller.remove_listener(listener)
 
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-ConsoleMotionRecorder
-=====================
+AirDrum
+=======
 
-Air drum with a Leap device
+Air drum with the Leap device
 
 ## Platforms
 
@@ -10,12 +10,31 @@ This should work under Windows, Linux and Mac, though only Windows was tested ye
 ## Requirements
 
 This tool requires a python 2.x installation and a Leap Motion device (www.leapmotion.com) with installed Leap app v0.7.3 or higher.
-Alongside with the source file *ConsoleMotionRecorder.py*, the Leap python API (again SDK v0.7.3 or higher) should be provided in the same directory:
+Alongside with the source file *AirDrum.py*, the Leap python API (again SDK v0.7.3 or higher) should be provided in the same directory:
 
 * *Leap.py*
 * *_LeapPython.pyd*
 * *Leap.dll*
 
+If no Midi output is used, there have to be four audio files in the same directory as well:
+
+* *Kick_soft.ogg*
+* *Kick_hard.ogg*
+* *Snare_soft.ogg*
+* *Snare_hard.ogg*
+
+If the Midi output want to be used instead, there has at least one Midi output device to be present.
+
 ## Usage
 
-Start the tool, connect the Leap and make sure the Leap app is running. Moving down a finger fast enough will trigger the MIDI note E4 on MIDI channel 0 with velocity depending on the speed of the finger.
+Start the tool, connect the Leap and make sure the Leap app is running. Moving down a finger/hand/tool fast enough will trigger a drum. If you hit on the left side of the Leap, a snare drum will be triggered. On the right side, a kick drum will be triggered. The volume is depending on the speed of the finger/hand/tool.
+
+### Midi
+
+If you selected to use a Midi output, the kick drum hit will be sent on note 36 (C1) and the snare drum hit on note 40 (E1).
+Note that if you require as little latency as possible during playing, you should absolutely select the Midi output instead of the audio output. 
+
+### Audio
+
+If you selected to use an audio output, there are two double sampled drum samples for each snare and kick drum. The *hard* sample will only play above a certain finger/hand/tool speed of the hit.
+If you notice sound playback failures, try setting the sample rate of the audio device initialization to a higher value (the last parameter of the function `pygame.mixer.pre_init()` in `DrumListener::init_audio()`)
